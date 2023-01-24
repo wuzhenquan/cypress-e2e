3e98e86fd40222c2fd419f7e2d2fbd2db35f32f2
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <div className="bg-white text-3xl text-center m-10 shadow-xl">
+        Hello World
+      </div>
+    </div>
+  );
+};
+
+export default App;
