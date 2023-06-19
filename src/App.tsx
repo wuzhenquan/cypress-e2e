5f7c8a6d3cb5f0b9974de2ac0c5b909b95fed9e0
巫镇泉 <zhenquan.me@gmail.com>
2023-06-19 09:37:31 +0800

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-const App = () => {
-  return (
-    <div>
-      <div className="bg-white text-3xl text-center m-10 shadow-xl">
-        Hello World
+import { useState } from 'react';
+
+const Template = () => {
+  const [username, setUsername] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    setSuccess(true);
+  };
+
+  if (success) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold text-green-500">Success!</h1>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <form onSubmit={handleSubmit} className="flex items-center justify-center h-screen">
+        <div className="border rounded-md p-4">
+          <h1 className="text-3xl font-bold mb-4">Login</h1>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="border border-gray-300 rounded py-2 px-3 w-full"
+          />
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded">
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
 };
 
-export default App;
+export default Template;
