7b7bd4c1c625524264e6dd045d3882cee69217a0
巫镇泉 <zhenquan.me@gmail.com>
2023-06-26 10:07:43 +0800

    feat:  中文化

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,15 +1,20 @@
 describe('template spec', () => {
   beforeEach(() => {
+    cy.viewport('iphone-6');
     cy.visit('http://localhost:5173');
   });
   
-  it('allows a user to sign up', () => {
-    cy.get('h1').should('contain.text', 'Login');
+  it('用户登录', () => {
+    // 查找 <h1> 标签，检查文本内容是否包含“登录”
+    cy.get('h1').should('contain.text', '登录');
     
-    cy.get('input').type('hi-mom');
+    // 查找 <input> 标签，将 wuzhenquan 输入到输入框中
+    cy.get('input').type('wuzhenquan');
 
+    // 查找 <button> 标签，单击它
     cy.get('button').click();
 
-    cy.get('h1').should('contain.text', 'Success!');
+    // 查找 <h1> 标签，检查文本内容是否包含“登录成功”
+    cy.get('h1').should('contain.text', '登录成功');
   })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,14 @@ const Template = () => {
   if (success) {
     return (
       <div className="flex items-center justify-center h-screen">
-        <h1 className="text-3xl font-bold text-green-500">Success!</h1>
+        <h1 className="text-3xl font-bold text-green-500">登录成功</h1>
       </div>
     );
   } else {
     return (
       <form onSubmit={handleSubmit} className="flex items-center justify-center h-screen">
         <div className="border rounded-md p-4">
-          <h1 className="text-3xl font-bold mb-4">Login</h1>
+          <h1 className="text-3xl font-bold mb-4">登录</h1>
           <input
             type="text"
             value={username}
